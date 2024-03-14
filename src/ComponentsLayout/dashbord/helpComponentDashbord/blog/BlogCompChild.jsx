d8119073635f5c4ae1b3d71989d134fce2d7f8b4
brahim <brahim@gmail.com>
2024-03-14 18:49:45 +0000

    add span inside the blog child div rotate -90 deg

--- src/ComponentsLayout/dashbord/helpComponentDashbord/blog/BlogCompChild.jsx
+++ src/ComponentsLayout/dashbord/helpComponentDashbord/blog/BlogCompChild.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 function BlogCompChild({ element }) {
   return (
-    <div className=" h-450px p-1 col-span-6 lg:col-span-2 xl:col-span-2 w-90%   bg-green-500 ">
+    <div className=" h-450px p-1 col-span-6 lg:col-span-2 xl:col-span-2 w-90%   bg-green-500 relative">
       <Image
         src="https://images.pexels.com/photos/20498979/pexels-photo-20498979/free-photo-of-mongolian-girl.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
         width={300}
@@ -11,6 +11,18 @@ function BlogCompChild({ element }) {
         className="h-80% w-100%"
         alt="Error 404"
       />
+
+      <span
+        className=" absolute z-3 bg-yellow-300 text-white w-100px "
+        style={{
+          top: "calc(50% ) ",
+          left: "83.5%",
+          transform: "rotate(-90deg)",
+        }}
+      >
+        {" "}
+        03-Mars-2000{" "}
+      </span>
       <h1> Title </h1>
       <p> Desc </p>
       <div className="mt-2">
